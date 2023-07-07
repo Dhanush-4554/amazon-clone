917bad4be8a7ad19f591ea620d59aee879fb7c32
Dhanush <118877804+Dhanush-4554@users.noreply.github.com>
2023-07-07 11:46:24 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -10,13 +10,7 @@ import "firebase/compat/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBg8FZqmdvzrxs3hGZPDOh1MI1Q1F2TDhA",
-    authDomain: "clone-ed385.firebaseapp.com",
-    projectId: "clone-ed385",
-    storageBucket: "clone-ed385.appspot.com",
-    messagingSenderId: "733545505347",
-    appId: "1:733545505347:web:00673e23200a8216fcaaf6",
-    measurementId: "G-GKDQETTTTY"
+   //api info
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
